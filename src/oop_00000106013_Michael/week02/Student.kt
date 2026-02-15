429265530c65749e52a690cf3dcd304943299352
Michael <michael34@student.umn.ac.id>
2026-02-15 22:57:03 +0700

    week02: implement selection logic for constructors

--- src/oop_00000106013_Michael/week02/Student.kt
+++ src/oop_00000106013_Michael/week02/Student.kt
@@ -1,21 +1,38 @@
 package oop_00000106013_Michael.week02
 
-class Student (
-    val name: String,
-    val nim: String,
-    var major: String,
-    var gpa: Double = 0.0  // <-- Default Argument
-) {
-    init {
-        if (nim.length != 5) {
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine() // Consume newline
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    } else {
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine() // Consume newline
+
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+            // Memanggil Primary Constructor
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+        } else if (type == 2) {
+            // Memanggil Secondary Constructor, Jurusan otomatis "Non-Matriculated"
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
         } else {
-            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+            println("Pilihan ngawur, pendaftaran batal!")
         }
     }
-
-    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
-        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
-    }
-}
