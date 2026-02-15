e47a670b44180e81ae064c29ec31dd5d7a8d304e
Michael <michael34@student.umn.ac.id>
2026-02-15 23:06:31 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000106013_Michael/week02/hero.kt
@@ -0,0 +1,23 @@
+package oop_00000106013_Michael.week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100  // Default Argument
+) {
+    fun attack(targetName: String) {
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        // Pastikan HP tidak minus
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
