06bdfa99722b98bd1f704a7746629812d113662c
Michael <michael34@student.umn.ac.id>
2026-02-15 22:27:10 +0700

    week01: add calculateStatus function

--- src/oop_00000106013_Michael/week01/GradingSystem.kt
+++ src/oop_00000106013_Michael/week01/GradingSystem.kt
@@ -3,13 +3,14 @@ package oop_00000106013_Michael.week01
 fun main(args: Array<String>) {
     val studentName = "John"
     val score = 85
-
     val grade = when {
         score >= 85 -> "A"
         score >= 70 -> "B"
         score >= 60 -> "C"
         else -> "D"
     }
-
-    println("Student: $studentName, Score: $score, Grade: $grade")
+    val status = calculateStatus(score)
+    println("Student: $studentName, Score: $score, Grade: $grade, Status: $status")
 }
+
+fun calculateStatus(score: Int) = if (score >= 60) "Lulus" else "Tidak Lulus"
