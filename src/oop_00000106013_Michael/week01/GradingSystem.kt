40eb7c17913aef994d5b37158df9dd66ec8c6023
Michael <michael34@student.umn.ac.id>
2026-02-15 22:26:31 +0700

    week01: refactor to use val and string templates

--- src/oop_00000106013_Michael/week01/GradingSystem.kt
+++ src/oop_00000106013_Michael/week01/GradingSystem.kt
@@ -1,7 +1,7 @@
 package oop_00000106013_Michael.week01
 
 fun main(args: Array<String>) {
-    var studentName: String = "John"
-    var score: Int = 85
-    println("Student: " + studentName + ", Score: " + score)
+    val studentName = "John"
+    val score = 85
+    println("Student: $studentName, Score: $score")
 }
