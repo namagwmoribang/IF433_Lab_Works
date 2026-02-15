c4b6a7ca1cd5761765659e4ffa5889746e16b3bc
Michael <michael34@student.umn.ac.id>
2026-02-15 22:55:09 +0700

    week02: add secondary constructor

--- src/oop_00000106013_Michael/week02/Student.kt
+++ src/oop_00000106013_Michael/week02/Student.kt
@@ -6,12 +6,17 @@ class Student (
     var major: String
 ) {
     init {
-        // Validasi Sederhana: Cek panjang NIM
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
+    }
+
+    // Secondary Constructor
+    // Wajib memanggil Primary Constructor menggunakan 'this()'
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }
