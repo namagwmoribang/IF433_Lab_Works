5fc0c3fc0810810e94a0e0b3e2048c0153a52dd6
Michael <michael34@student.umn.ac.id>
2026-02-25 10:19:24 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000106013_Michael/week03/Employee.kt
+++ src/oop_00000106013_Michael/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_00000106013_Michael.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // PERINGATAN: KODE INI SALAH & BERBAHAYA!
+            this.salary = value  // Ini akan recursive!
+        }
 }
