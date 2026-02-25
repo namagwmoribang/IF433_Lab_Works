07cd0a158c8ecca9ba586c1077babef55b744c0f
Michael <michael34@student.umn.ac.id>
2026-02-25 10:23:37 +0700

    week03: cleanup code

--- src/oop_00000106013_Michael/week03/Employee.kt
+++ src/oop_00000106013_Michael/week03/Employee.kt
@@ -1,28 +1,11 @@
 package oop_00000106013_Michael.week03
 
-class Employee(val name: String) {
-    var salary: Int = 0
-        set(value) {
-            if (value < 0) {
-                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0
-            } else {
-                field = value
-            }
+var salary: Int = 0
+    set(value) {
+        if (value < 0) {
+            // println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+            field = 0
+        } else {
+            field = value
         }
-
-    private var performanceRating: Int = 3
-
-    fun increasePerformance() {
-        performanceRating++
-        println("Kinerja $name meningkat! Rating: $performanceRating")
     }
-
-    fun printStatus() {
-        println("Karyawan: $name, Rating: $performanceRating")
-    }
-
-    // Computed Property - dihitung setiap kali diakses
-    val tax: Double
-        get() = salary * 0.1
-}
