67558e75269927a38b8e415b56acb2c12b855dcd
Michael <michael34@student.umn.ac.id>
2026-03-04 10:55:11 +0700

    week04: (task 2) create Developer class

---
+++ src/oop_00000106013_Michael/week04/Developer.kt
@@ -0,0 +1,14 @@
+package oop_00000106013_Michael.week04
+
+class Developer(
+    name: String,
+    baseSalary: Int,
+    val programmingLanguage: String
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+
+    // calculateBonus() TIDAK di-override, jadi pakai bawaan Parent (10%)
+}
