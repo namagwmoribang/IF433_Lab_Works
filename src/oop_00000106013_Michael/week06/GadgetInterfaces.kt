07447d58e8ed974b8cbdd6da73c9c2b98d054013
Michael <michael34@student.umn.ac.id>
2026-03-18 23:45:07 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000106013_Michael/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000106013_Michael.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
