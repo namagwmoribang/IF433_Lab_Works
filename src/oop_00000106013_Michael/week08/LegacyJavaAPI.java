5ccfe0fb7f3e473a60acb4429d03258e318b40b1
Michael <michael34@student.umn.ac.id>
2026-04-22 10:25:56 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000106013_Michael/week08/LegacyJavaAPI.java
@@ -0,0 +1,9 @@
+package oop_00000106013_Michael.week08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya
+    // Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
