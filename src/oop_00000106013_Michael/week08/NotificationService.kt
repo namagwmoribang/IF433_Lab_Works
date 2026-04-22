f687101c2c55e1b6952d68e6ef5a5eba0580971c
Michael <michael34@student.umn.ac.id>
2026-04-22 10:16:39 +0700

    week08: implement smart cast in function

---
+++ src/oop_00000106013_Michael/week08/NotificationService.kt
@@ -0,0 +1,19 @@
+package oop_00000106013_Michael.week08
+
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        // sendEmail(user.email)  // INI AKAN ERROR: Type mismatch
+
+        if (user.email != null) {
+            // SUCCESS via Smart Cast: compiler tahu 'user.email' pasti tidak
+            // null di blok ini
+            sendEmail(emailAddress = user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
