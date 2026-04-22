7c0dec1d2cf3f8d8dc2a3b13f35b8bf14e97d03c
Michael <michael34@student.umn.ac.id>
2026-04-22 10:28:40 +0700

    week08: simulate unit test failure using !!

--- src/oop_00000106013_Michael/week08/Main.kt
+++ src/oop_00000106013_Michael/week08/Main.kt
@@ -62,6 +62,7 @@ fun main() {
                     // Kita menekan tombol !! karena KITA TAHU implementasi Java-nya aman
                     val statusLength = javaResponse!!.length
                     println("Status dari Java: $javaResponse (Length: $statusLength)")
+                    runMockUnitTest()
                 }
             }
         }
