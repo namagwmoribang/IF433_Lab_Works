c0b710a249b520f5734b808b511cc3f556a10f4a
Michael <michael34@student.umn.ac.id>
2026-04-22 10:32:55 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000106013_Michael/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000106013_Michael.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
