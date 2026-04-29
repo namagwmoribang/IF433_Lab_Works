606d8a7a9c91fbdf5e650edbcf571deffa26cc72
Michael <michael34@student.umn.ac.id>
2026-04-29 10:51:54 +0700

    week09: (task) extract unique trading pairs using map and toSet

--- src/oop_00000106013_Michael/week09/TradingDashBoard.kt
+++ src/oop_00000106013_Michael/week09/TradingDashBoard.kt
@@ -32,4 +32,7 @@ fun main() {
         .sortedBy { it.roe }
         .map { "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
 
+    val uniquePairs = tradeHistory
+        .map { it.pair }
+        .toSet()
 }
