34b5e49314c327f20a009ce803488ca23514bf9e
Michael <michael34@student.umn.ac.id>
2026-04-29 10:39:11 +0700

    week09: create Student data class and list

---
+++ src/oop_00000106013_Michael/week09/ObjectChaining.kt
@@ -0,0 +1,16 @@
+package oop_00000106013_Michael.week09
+
+data class Student(val name: String, val gpa: Double)
+
+fun main() {
+    val students = listOf(
+        Student("Andi", 3.2),
+        Student("Budi", 3.9),
+        Student("Citra", 2.8),
+        Student("Dewi", 3.7)
+    )
+
+    println("All Students:")
+    students.forEach { println("  ${it.name} - GPA: ${it.gpa}") }
+
+}
