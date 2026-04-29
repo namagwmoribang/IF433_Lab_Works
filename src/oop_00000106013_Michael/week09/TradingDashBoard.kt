f46cb574bb6873f9250531df82ca4833bb239e33
Michael <michael34@student.umn.ac.id>
2026-04-29 10:49:14 +0700

    week09: (task) chain sortedByDescending and map for top performers

--- src/oop_00000106013_Michael/week09/TradingDashBoard.kt
+++ src/oop_00000106013_Michael/week09/TradingDashBoard.kt
@@ -23,4 +23,9 @@ fun main() {
 
     val losingTrades = closedTrades
         .filter { it.roe <= 0 }
+
+    val topPerformersString = winningTrades
+        .sortedByDescending { it.roe }
+        .map { "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
+
 }
