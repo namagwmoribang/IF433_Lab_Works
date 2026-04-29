7af51d9402974fcb2d4b506cc589ecb3e08a41c1
Michael <michael34@student.umn.ac.id>
2026-04-29 10:27:49 +0700

    week09: create immutable and mutable list

--- src/oop_00000106013_Michael/week09/CollectionsBasics.kt
+++ src/oop_00000106013_Michael/week09/CollectionsBasics.kt
@@ -2,36 +2,13 @@ package oop_00000106013_Michael.week09
 
 fun main() {
     println("=== TEST LIST ===")
-    // Immutable List: Tidak bisa diubah setelah dibuat
+
     val frameworks: List<String> = listOf("Kotlin", "Java", "C++")
-    // frameworks.add("Python") // UNCOMMENT INI AKAN ERROR!
+
     println("Immutable List: $frameworks")
 
-    // Mutable List: Bisa ditambah/dikurangi
     val scores: MutableList<Int> = mutableListOf(85, 90)
     scores.add(95)
     scores[0] = 88
     println("Mutable List: $scores")
-
-    println("\n=== TEST SET ===")
-    val uniqueNumbers = setOf(1, 2, 2, 3, 3, 4)
-    println("Unique Numbers (Set): $uniqueNumbers") // Duplikat hilang
-
-    val activeUsers = mutableSetOf("UserA", "UserB")
-    activeUsers.add("UserC")
-    activeUsers.add("UserA") // Diabaikan karena sudah ada
-    println("Active Users: $activeUsers")
-
-    println("\n=== TEST MAP ===")
-    val studentGrades = mapOf(
-        "Alice" to "A",
-        "Bob" to "B",
-        "Charlie" to "A" // Value boleh duplikat, Key tidak
-    )
-    println("Nilai Bob: ${studentGrades["Bob"]}")
-
-    val inventory = mutableMapOf("Apples" to 50, "Bananas" to 30)
-    inventory["Oranges"] = 20 // Menambah data baru
-    inventory["Apples"] = 45  // Mengupdate data lama
-    println("Inventory: $inventory")
 }
