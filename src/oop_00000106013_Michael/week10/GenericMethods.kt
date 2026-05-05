669ad8ffbc74db502aa10f872614e5202a4454fd
Michael <michael34@student.umn.ac.id>
2026-05-06 00:31:11 +0700

    week10: implement where clause for comparable constraints

--- src/oop_00000106013_Michael/week10/GenericMethods.kt
+++ src/oop_00000106013_Michael/week10/GenericMethods.kt
@@ -1,9 +1,15 @@
 package oop_00000106013_Michael.week10
 
+package oop_001_johnthor.week10
+
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
 }
 
 fun <T> processData(input: T): T {
     return input
+}
+
+fun <T> getMax(a: T, b: T): T where T : Comparable<T> {
+    return if (a > b) a else b
 }
