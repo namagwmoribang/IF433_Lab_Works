ff028279916f16543b4c85b96af6feb6818a6870
Michael <michael34@student.umn.ac.id>
2026-05-06 00:40:55 +0700

    week10: (task) populate coin repository

--- src/oop_00000106013_Michael/week10/CryptoDashboard.kt
+++ src/oop_00000106013_Michael/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000106013_Michael.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
