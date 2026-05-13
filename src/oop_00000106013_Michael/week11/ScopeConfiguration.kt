03cb15a75b1c271fbbbc9f4d073f5c20257b4c24
Michael <michael34@student.umn.ac.id>
2026-05-13 22:34:17 +0700

    week11: implement apply for object initialization

--- src/oop_00000106013_Michael/week11/ScopeConfiguration.kt
+++ src/oop_00000106013_Michael/week11/ScopeConfiguration.kt
@@ -1,3 +1,12 @@
 package oop_00000106013_Michael.week11
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
+}
