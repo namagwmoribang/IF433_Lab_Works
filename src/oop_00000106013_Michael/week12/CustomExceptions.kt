cf6a000de8ad77df6d90976c23a5ab698cf51a18
Michael <michael34@student.umn.ac.id>
2026-05-17 22:09:32 +0700

    week12: create InsufficientFunds exception

---
+++ src/oop_00000106013_Michael/week12/CustomExceptions.kt
@@ -0,0 +1,7 @@
+package oop_00000106013_Michael.week12
+
+// Custom exception dengan membawa data tambahan
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception("Attempted $amount, balance: $balance")
