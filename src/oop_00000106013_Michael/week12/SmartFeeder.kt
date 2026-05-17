2c55ce6e023a2d34126a97b7877c333663c461dc
Michael <michael34@student.umn.ac.id>
2026-05-17 22:23:41 +0700

    week12: (task) implement dispenseKibble function

--- src/oop_00000106013_Michael/week12/SmartFeeder.kt
+++ src/oop_00000106013_Michael/week12/SmartFeeder.kt
@@ -2,4 +2,13 @@ package oop_00000106013_Michael.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    require(availableGram >= 0) { "Gram yang tersedia tidak boleh negatif" }
+
+    if (isJammed) return 0
+
+    return if (requestedGram <= availableGram) {
+        requestedGram
+    } else {
+        availableGram
+    }
 }
