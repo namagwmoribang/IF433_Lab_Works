723ff2ce8c3b1b2ce26f5a2076152e05294f89cb
Michael <michael34@student.umn.ac.id>
2026-05-17 22:29:31 +0700

    week12: (task) implement finally block for feeder logging

--- src/oop_00000106013_Michael/week12/SmartFeeder.kt
+++ src/oop_00000106013_Michael/week12/SmartFeeder.kt
@@ -22,5 +22,7 @@ fun main() {
         println("Caught Domain Error: Stok tidak cukup. ${e.message}")
     } catch (e: Exception) {
         println("Caught General Error: Terjadi kesalahan tidak terduga.")
-    }
+    } finally {
+    println("Siklus pengecekan dispenser pagi selesai.")
+}
 }
