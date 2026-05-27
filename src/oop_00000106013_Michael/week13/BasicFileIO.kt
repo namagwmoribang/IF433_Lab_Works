f80f5a33cfe23ca88e981b1f52d4d04d35fd18b6
Michael <michael34@student.umn.ac.id>
2026-05-27 14:44:53 +0700

    week13: implement basic writeText function

--- src/oop_00000106013_Michael/week13/BasicFileIO.kt
+++ src/oop_00000106013_Michael/week13/BasicFileIO.kt
@@ -7,17 +7,4 @@ fun main() {
     val file = File("notes.txt")
     file.writeText("Line 1: Inisialisasi sistem.\n")
     println("File berhasil dibuat dan ditulis.")
-
-    file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
-    println("Teks berhasil di-append.")
-
-    println("\n=== TEST READ TEXT ===")
-    val fullContent = file.readText() // Mengambil seluruh isi file sebagai satu String
-    println("Membaca sekaligus:\n$fullContent")
-
-    println("=== TEST READ LINES ===")
-    val lines = file.readLines() // Mengambil per baris sebagai List<String>
-    lines.forEachIndexed { index, line ->
-        println("Isi Index $index: $line")
-    }
 }
